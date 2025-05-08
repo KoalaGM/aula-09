0184b9088ab7f1e167da935960ed289396578efb
ANALISTA DE DADOS - BIG DATA SCIENCE 2025.1 <51134552025.1@senacrjedu.local>
2025-05-08 19:16:50 -0300

    Aula 09

---
+++ aula-9/exemplo_for_while.py
@@ -0,0 +1,17 @@
+# lst_valores = []
+
+
+# for i in range(5):
+#     num = int(input('Informe o numero: '))
+#     lst_valores.append(num)
+
+# print(f'Os números são {lst_valores}')
+
+
+# While
+lst_nomes = []
+resposta = ''
+
+while resposta != 'n':
+    nome = input('Informe o nome: ')
+    lst_nomes.append(nome)
